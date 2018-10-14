a1c784bc0664339a9e1660368d0b6e1f9a9a043c
Emir Salkanovic <emirsalkanovic1@gamil.com>
2018-10-14 22:43:45 +0200

    update gitignore

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import API_KEY from '../keys/api_key';
 
 export const RECEIVE_MOVIES = 'RECEIVE_MOVIES';
 export const RECEIVE_TV = 'RECEIVE_TV';
 export const SEARCH_MOVIES = 'SEARCH_MOVIES';
 export const SEARCH_SHOWS = 'SEARCH_SHOWS';
 
-const API_KEY = '540ae0350b2b2b29fea22a8a8552135e';
+
 const ROOT_URL = 'https://api.themoviedb.org/3/movie/top_rated?page=1&language=en-US&api_key=';
 const MOVIE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&include_adult=false&query=`;
 const TV_URL = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&language=en-US&query=`;
@@ -29,7 +30,7 @@ export function topMovies() {
 export function topTv(){
 	return (dispatch) => {
 	axios
-	  .get('https://api.themoviedb.org/3/tv/top_rated?api_key=540ae0350b2b2b29fea22a8a8552135e&language=en-US&page=1')
+	  .get(`https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
 	  .then((res) => {
 	  		const results = res.data.results;
 
