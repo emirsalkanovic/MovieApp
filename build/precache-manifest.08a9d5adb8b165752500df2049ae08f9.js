b55b6427dcff30c978c1d9ef9034067707803bd8
Emir Salkanovic <emirsalkanovic1@gamil.com>
2019-04-11 16:59:20 +0200

    deployment setup

---
+++ build/precache-manifest.08a9d5adb8b165752500df2049ae08f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9b8d78f6d45c181a7bb44d2de9aab8e",
+    "url": "/static/media/bg-movie.c9b8d78f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46a03fc39f54bc31526c",
+    "url": "/static/js/main.46a03fc3.chunk.js"
+  },
+  {
+    "revision": "3f55512c194f71bfca99",
+    "url": "/static/js/1.3f55512c.chunk.js"
+  },
+  {
+    "revision": "46a03fc39f54bc31526c",
+    "url": "/static/css/main.70c8d68c.chunk.css"
+  },
+  {
+    "revision": "823ce40d568b5a7453e1164059845ac7",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const favicon = require('express-favicon');
 const path = require("path");
 const port = process.env.PORT || 3000;
+
 const app = express();
+app.use(favicon(__dirname + '/build/favicon.jpg'));
 
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) =>{
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
